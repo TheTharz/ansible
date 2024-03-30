50394bc292f239028a530c8f337981f4c50359a6
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-03-30 10:55:47 +0530

    Refactor code to improve performance and readability

---
+++ webscript.sh
@@ -0,0 +1,29 @@
+echo
+
+# Creating temp directory
+echo "#################################"
+echo "Starting artifact deployement"
+echo "#################################"
+mkdir -p /tmp/webfiles
+cd /tmp/webfiles
+echo
+
+wget https://www.tooplate.com/zip-templates/2098_health.zip
+unzip 2098_health.zip > /dev/null
+sudo cp -r 2098_health/* /var/www/html/
+
+# Bounce service
+echo "################################"
+echo "Restarting HTTPD service"
+echo "################################"
+systemctl restart httpd
+echo
+
+# Cleanup
+echo "###############################"
+echo "Removing temporary files"
+echo "###############################"
+rm -rf /tmp/webfiles
+echo
+
+sudo systemctl
